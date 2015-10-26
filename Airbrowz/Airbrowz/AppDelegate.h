ba0c3b96586eff6ae080ff24cf13500c65616fab
Yong Ro Park <yongro8912@gmail.com>
2015-10-26 00:37:51 -0400

    project init

---
+++ Airbrowz/Airbrowz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Airbrowz
+//
+//  Created by Denny on 2015-10-26.
+//  Copyright © 2015 Kiwi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
